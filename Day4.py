d01ad8cf09c7849d9d00d333bb694d6a1e5eaac6
AasishSapkota <54069703+AasishSapkota@users.noreply.github.com>
2024-04-12 23:55:00 +0545

    Day4.py

---
+++ Day4.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print(" CHecking if it is new line or not")
+print(" Testing if it prints new line /n it is second part of the same line ")
+print(2+3)
+a=5
+b=49
+c=a+b
+print(c)
