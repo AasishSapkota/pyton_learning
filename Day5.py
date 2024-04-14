10513274b88dd9ff8eb475e0949cfc3b40de9a7e
AasishSapkota <54069703+AasishSapkota@users.noreply.github.com>
2024-04-14 11:03:02 +0545

    Day5.py

---
+++ Day5.py
@@ -0,0 +1,9 @@
+# it is used for doing  single line comment
+'''It is multi line comment
+as you can see we are writing in second line also'''
+# escape seqence = \n for next line
+print("hellow bro \n what's up ")
+print(2,3,4,5)
+print(5+9)
+#sep="something" it is used when we need add space between data.
+print("aasish","sapkota",sep="*")
